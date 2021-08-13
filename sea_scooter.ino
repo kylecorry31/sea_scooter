84fc353fe03db7c8066691b3533f34e2379f965e
Kyle Corry <kylecorry31@gmail.com>
2021-08-13 15:49:47 -0400

    Make both buttons turn on the motors

--- sea_scooter.ino
+++ sea_scooter.ino
@@ -1,39 +1,41 @@
 #include <Servo.h>
 
 Servo ESC;
-int leftBtn = 3;
-int rightBtn = 4;
+int btn = 4;
 
-void setup() {
-  pinMode(leftBtn, INPUT_PULLUP);
-  pinMode(rightBtn, INPUT_PULLUP);
+void setup()
+{
+  pinMode(btn, INPUT_PULLUP);
   ESC.attach(10);
 }
 
-void loop() {
-    bool halfPower = !digitalRead(leftBtn) ^ !digitalRead(rightBtn);
-    bool fullPower = !digitalRead(leftBtn) && !digitalRead(rightBtn);
-    if (fullPower){
-      forward(100);
-    } else if (halfPower){
-      forward(50);
-    } else {
-        stop();
-    }
+void loop()
+{
+  if (!digitalRead(btn))
+  {
+    forward(100);
+  }
+  else
+  {
+    stop();
+  }
 }
 
-void calibrate(){
+void calibrate()
+{
   ESC.writeMicroseconds(2300);
   delay(5000);
   ESC.writeMicroseconds(1100);
-  delay(5000);  
+  delay(5000);
 }
 
-void forward(double power){
+void forward(double power)
+{
   double microseconds = map(power, 0, 100, 1100, 2300);
   ESC.writeMicroseconds(microseconds);
 }
 
-void stop(){
-  forward(0);  
+void stop()
+{
+  forward(0);
 }
