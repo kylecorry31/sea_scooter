1e96d485687daba77d84d3c7155aa44058f76a64
Kyle Corry <kylecorry31@gmail.com>
2022-06-06 17:49:39 -0400

    Extract constant values

--- sea_scooter.ino
+++ sea_scooter.ino
@@ -1,19 +1,32 @@
 #include <Servo.h>
 
 Servo ESC;
-int btn = 4;
+int btnPin = 4;
+int escPin = 10;
+
+int minimumMs = 1100;
+int maximumMs = 2300;
+int armMs = 500;
+int stopMs = minimumMs;
+int calibrationDelay = 5000;
+
+int forwardSpeed = 100;
 
 void setup()
 {
-  pinMode(btn, INPUT_PULLUP);
-  ESC.attach(10);
+  pinMode(btnPin, INPUT_PULLUP);
+  ESC.attach(escPin);
+  if (!digitalRead(btnPin)){
+    calibrate();
+  }
+  arm();
 }
 
 void loop()
 {
-  if (!digitalRead(btn))
+  if (!digitalRead(btnPin))
   {
-    forward(100);
+    forward(forwardSpeed);
   }
   else
   {
@@ -23,19 +36,24 @@ void loop()
 
 void calibrate()
 {
-  ESC.writeMicroseconds(2300);
-  delay(5000);
-  ESC.writeMicroseconds(1100);
-  delay(5000);
+  ESC.writeMicroseconds(maximumMs);
+  delay(calibrationDelay);
+  ESC.writeMicroseconds(minimumMs);
+  delay(calibrationDelay);
+}
+
+void arm(){
+  ESC.writeMicroseconds(armMs);
 }
 
 void forward(double power)
 {
-  double microseconds = map(power, 0, 100, 1100, 2300);
+  double microseconds = map(power, 0, 100, minimumMs, maximumMs);
+  microseconds = constrain(microseconds, minimumMs, maximumMs);
   ESC.writeMicroseconds(microseconds);
 }
 
 void stop()
 {
-  forward(0);
+  ESC.writeMicroseconds(stopMs);
 }
